d7fc31693cafd88bc54e00e27f5c51a45d2365ac
bharat1704 <bharatmali1704@gmail.com>
2025-03-11 09:08:07 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req,res)=> {
+        res.send("hello from simple route")
+
+    });
+
+app.get("/cpu", (req,res)=>{
+    
+    for(let i=0;i<100000000;i++){
+    Math.random();
+    }
+    
+    res.send("hello world bharat")
+
+})
+
+app.listen(3000)
